5a37ecaee6d1d7931bae73f0da09c11f55290524
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-01-09 22:49:59 +0530

    feat(Category): added individual screens for category

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -8,7 +8,7 @@ const Billboard = ({ data }: { data: Billboard }) => {
         className='rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover'
       >
         <div className='h-full w-full flex flex-col justify-center items-center text-center gap-y-8'>
-          <div className='font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs'>{data.label}</div>
+          <div className='font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs text-white'>{data.label}</div>
         </div>
       </div>
     </div>
