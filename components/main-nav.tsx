4c75f39298cd12d9366c83f3e8d6e2dd9d6a6820
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-01-09 21:07:39 +0530

    feat(Setup): Setup and landing page added

---
+++ components/main-nav.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
+import { Category } from '@/types';
+
+const MainNav = ({ data }: { data: Category[] }) => {
+  const pathname = usePathname();
+
+  const routes = data.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category/${route.id}`,
+  }));
+
+  return (
+    <nav className='mx-6 flex items-center space-x-4 lg:space-x-6'>
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn('text-sm font-medium transition-colors hover:text-black', route.active ? 'text-black' : 'text-neutral-500')}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default MainNav;
