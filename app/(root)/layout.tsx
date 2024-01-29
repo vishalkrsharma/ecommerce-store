99ec614a312922a227522329645df0cdad264f28
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-01-29 22:42:06 +0530

    feat(Auth): add

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import Footer from '@/components/footer';
+import Navbar from '@/components/navbar';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { PropsWithChildren } from 'react';
+
+const SetupLayout = async ({ children }: PropsWithChildren) => {
+  const { userId } = auth();
+
+  if (!userId) redirect('/sign-in');
+
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default SetupLayout;
